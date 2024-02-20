165609f1b16a387089fd751164533c8278dda491
Abhinav shyju <96369574+abhinavshyju@users.noreply.github.com>
2024-02-20 21:34:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       
-        <Route path='' element={<HomeScreen/>}/>
+        <Route path='/' element={<HomeScreen/>}/>
         <Route path='/sports' element={<SportsResult/>}/>
     </Routes>
     </BrowserRouter>
